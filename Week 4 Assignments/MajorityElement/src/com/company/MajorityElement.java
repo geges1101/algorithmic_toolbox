1d7893559af1e3dcf28d0a118081cb8c710546c4
geges1101 <62469977+geges1101@users.noreply.github.com>
2021-02-14 14:09:42 +0300

    Add files via upload

---
+++ Week 4 Assignments/MajorityElement/src/com/company/MajorityElement.java
@@ -0,0 +1,73 @@
+//package com.company;
+
+import java.util.*;
+import java.io.*;
+
+public class MajorityElement {
+    private static int getMajorityElement(int[] a) {
+        int major = a[0];
+        int count = 1;
+
+        for (int i = 1; i < a.length; i++){
+            if(major == a[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count == 0 && i!= a.length - 1){
+                major = a[i];
+                count++;
+            }
+        }
+        if (count != 0){
+            count = 0;
+            for (int j : a) {
+                if (j == major) count++;
+            }
+            if(count > a.length / 2)  return major;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        if (getMajorityElement(a) != -1) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
